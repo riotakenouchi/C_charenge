0484b494adff47838501b638dbb74b80aa2bd7a7
unknown <rio_takenouchi@S23994.hulft.saison.co.jp>
2024-07-17 17:06:33 +0900

    Re try2

--- exercise1/practice_1.c
+++ exercise1/practice_1.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 int main(void) {
     int num = 20;
-    printf("10進数=%d 16進数=%#X 8進数=%#o", num, num, num);
+    printf("10進数=%d 16進数=%#X 8進数=%#o\n", num, num, num);
     return 0;
 }
+
+
