f762bd20512113aca673ec393dd2a7907e7b192b
riotakenouchi <rio_takenouchi@saison-technology.com>
2024-07-19 16:36:32 +0900

    課題2のコードと実行結果 (#4)
    * 課題2のコードと実行結果
    
    * Re try1
    
    ---------
    
    Co-authored-by: unknown <rio_takenouchi@S23994.hulft.saison.co.jp>

---
+++ exercise2/practice_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    unsigned int size_char = sizeof(char);
+    unsigned int size_short = sizeof(short);
+    unsigned int size_int = sizeof(int);
+    unsigned int size_long = sizeof(long);
+    unsigned int size_float = sizeof(float);
+    unsigned int size_double = sizeof(double);
+    printf("char=%05u short=%05u int=%05u long=%05u float=%05u double=%05u\n",
+           size_char, size_short, size_int, size_long, size_float, size_double);
+    return 0;
+}
