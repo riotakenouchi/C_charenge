72e64879d56234e309a4b608b16e8947b50d5b42
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-08-08 17:05:47 +0900

    Retry3

--- exercise5/practice_5.c
+++ exercise5/practice_5.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-
+int main(void) 
+{
     /* 配列の定義 */
-    char char_array[10];
-    short short_array[10];
-    int int_array[10];
-    long long_array[10];
+    char char_array[2] = 0x00;
+    short short_array[2] = 0x00;
+    int int_array[2] = 0x00;
+    long long_array[2] = 0x00;
 
     /* ポインタの定義 */
     char *char_ptr = char_array;
@@ -14,15 +14,13 @@ int main(void) {
     int *int_ptr = int_array;
     long *long_ptr = long_array;
 
-    /* 配列の添字を進めた場合のアドレスの差の出力 */
+    /* 配列の添字を進めた場合のアドレスの差とポインタをインクリメントした場合のアドレスの差の出力 */
     printf("char 配列=%zu short 配列=%zu int 配列=%zu long 配列=%zu\n",
+           "charポインタ=%zu shortポインタ=%zu intポインタ=%zu longポインタ=%zu\n",
            (size_t)(&char_array[1]) - (size_t)(&char_array[0]),
            (size_t)(&short_array[1]) - (size_t)(&short_array[0]),
            (size_t)(&int_array[1]) - (size_t)(&int_array[0]),
-           (size_t)(&long_array[1]) - (size_t)(&long_array[0]));
-
-    /* ポインタをインクリメントした場合のアドレスの差の出力 */
-    printf("charポインタ=%zu shortポインタ=%zu intポインタ=%zu longポインタ=%zu\n",
+           (size_t)(&long_array[1]) - (size_t)(&long_array[0]),
            (size_t)(char_ptr + 1) - (size_t)(char_ptr),
            (size_t)(short_ptr + 1) - (size_t)(short_ptr),
            (size_t)(int_ptr + 1) - (size_t)(int_ptr),
