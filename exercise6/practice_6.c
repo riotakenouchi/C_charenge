6f8818d8280e83a51c42a0b8672cdef980c97f99
riotakenouchi <rio_takenouchi@saison-technology.com>
2024-08-14 14:51:43 +0900

    課題6のコードと実行結果 (#9)
    * 課題6のコードと実行結果
    
    * Retry1
    
    * Retry2
    
    * Retry3
    
    ---------
    
    Co-authored-by: rio_takenouchi <rio_takenouhci@saison-technology.com>

---
+++ exercise6/practice_6.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stddef.h>  /*offsetof を使用するため定義*/
+
+struct SAMPLE_t
+ {
+    char m1;
+    short m2;
+    char m3;
+    long m4;
+    char m5;
+    void *m6;
+};
+
+int main(void) 
+{
+    struct SAMPLE_t sample;  /*SAMPLE_t のインスタンスを作成*/
+
+    /*各メンバのサイズを格納するための変数の宣言と初期化処理*/
+    size_t size_m1 = sizeof(sample.m1); 
+    size_t size_m2 = sizeof(sample.m2); 
+    size_t size_m3 = sizeof(sample.m3);
+    size_t size_m4 = sizeof(sample.m4); 
+    size_t size_m5 = sizeof(sample.m5); 
+    size_t size_m6 = sizeof(sample.m6);
+    size_t total_size = size_m1 + size_m2 + size_m3 + size_m4 + size_m5 + size_m6;
+
+    /*構造体のサイズと各メンバサイズの合計を表示*/
+    printf("構造体のサイズ=%zu メンバの合計サイズ=%zu\n", sizeof(struct SAMPLE_t),total_size);
+
+    /*各メンバのオフセットとサイズの出力*/
+    printf("char  m1 オフセット=%zu サイズ=%zu\n"
+           "short m2 オフセット=%zu サイズ=%zu\n"
+           "char  m3 オフセット=%zu サイズ=%zu\n"
+           "long  m4 オフセット=%zu サイズ=%zu\n"
+           "char  m5 オフセット=%zu サイズ=%zu\n"
+           "void* m6 オフセット=%zu サイズ=%zu\n",
+           offsetof(struct SAMPLE_t, m1), size_m1,
+           offsetof(struct SAMPLE_t, m2), size_m2,
+           offsetof(struct SAMPLE_t, m3), size_m3,
+           offsetof(struct SAMPLE_t, m4), size_m4,
+           offsetof(struct SAMPLE_t, m5), size_m5,
+           offsetof(struct SAMPLE_t, m6), size_m6);
+
+    return 0;
+}
