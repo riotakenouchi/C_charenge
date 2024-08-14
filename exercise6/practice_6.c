29aa01e035a7cea01c27cfd38cda9d2041c9672c
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-08-14 14:18:06 +0900

    Retry2

--- exercise6/practice_6.c
+++ exercise6/practice_6.c
@@ -3,7 +3,6 @@
 
 struct SAMPLE_t
  {
-    
     char m1;
     short m2;
     char m3;
@@ -12,36 +11,20 @@ struct SAMPLE_t
     void *m6;
 };
 
-int main(void) 
-{
-    
+int main(void) {
     struct SAMPLE_t sample;  /*SAMPLE_t のインスタンスを作成*/
 
-    /*各メンバのサイズを格納するための変数の宣言と初期化*/
-    size_t size_m1 = 0; 
-    size_t size_m2 = 0; 
-    size_t size_m3 = 0; 
-    size_t size_m4 = 0; 
-    size_t size_m5 = 0; 
-    size_t size_m6 = 0; 
-    size_t total_size = 0;
+    /*各メンバのサイズを格納するための変数の宣言と初期化処理*/
+    size_t size_m1 = sizeof(sample.m1); 
+    size_t size_m2 = sizeof(sample.m2); 
+    size_t size_m3 = sizeof(sample.m3);
+    size_t size_m4 = sizeof(sample.m4); 
+    size_t size_m5 = sizeof(sample.m5); 
+    size_t size_m6 = sizeof(sample.m6);
+    size_t total_size = size_m1 + size_m2 + size_m3 + size_m4 + size_m5 + size_m6;
 
-    /*メンバサイズの計算*/
-    size_m1 = sizeof(sample.m1);
-    size_m2 = sizeof(sample.m2);
-    size_m3 = sizeof(sample.m3);
-    size_m4 = sizeof(sample.m4);
-    size_m5 = sizeof(sample.m5);
-    size_m6 = sizeof(sample.m6);
-
-    /*各メンバサイズの合計*/
-    total_size = size_m1 + size_m2 + size_m3 + size_m4 + size_m5 + size_m6;
-
-    /*構造体のサイズ*/
-    printf("構造体のサイズ=%zu ", sizeof(struct SAMPLE_t));
-
-    /*各メンバサイズの合計を表示*/
-    printf("メンバの合計サイズ=%zu\n", total_size);
+    /*構造体のサイズと各メンバサイズの合計を表示*/
+    printf("構造体のサイズ=%zu メンバの合計サイズ=%zu\n", sizeof(struct SAMPLE_t),total_size);
 
     /*各メンバのオフセットとサイズの出力*/
     printf("char  m1 オフセット=%zu サイズ=%zu\n"
