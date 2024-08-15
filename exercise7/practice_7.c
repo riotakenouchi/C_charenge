858a33f3055e2a95a97313e0a53cd17cd3b4d6b2
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-08-15 13:47:15 +0900

    Retry3

--- exercise7/practice_7.c
+++ exercise7/practice_7.c
@@ -4,26 +4,23 @@
  * @brief カウンタを返す関数
  *
  * @details この関数は、プログラムの実行中に呼び出されるたびに
- * 1増加する静的なカウンタを返すことを示す
+            1増加する静的なカウンタを返すことを示す
  * 
  * @return int インクリメントされたカウンタの値
  */
-static int static_counter();
-
-int main(void) 
-{
-    /*カウンタの出力*/
-    int i;
-    for (int i = 0; i < 10; i++) {
-        int count_value = static_counter();  /*戻り値を変数に格納*/
-        printf("カウンタ: %d\n", count_value);
-    }
-
-    return 0;
-}
-
 static int static_counter()
 {
     static int count = 0; 
     return ++count;     
+}
+
+int main(void) 
+{
+    int i;
+    for (i = 0; i < 10; i++) {
+        int count_value = static_counter();  
+        printf("カウンタ: %d\n", count_value);
+    }
+
+    return 0;
 }
