ea74d7cfb2e35d2baf5d412520a150aa0ebafa7a
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-08-15 10:42:37 +0900

    Retry1

--- exercise7/practice_7.c
+++ exercise7/practice_7.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
-int static_counter();
+/*
+ * @brief カウンタを返す関数
+ *
+ * この関数は、プログラムの実行中に呼び出されるたびに
+ * 1増加する静的なカウンタを返すことを示す
+ * 
+ * @return int インクリメントされたカウンタの値
+ */
+static int static_counter();
 
 int main(void) 
 {
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    printf("カウンタ: %d\n", static_counter()); 
-    
+    /*カウンタの出力*/
+    for (int i = 0; i < 10; i++) {
+        int count_value = static_counter();  /*戻り値を変数に格納*/
+        printf("カウンタ: %d\n", count_value);
+    }
+
     return 0;
 }
 
-int static_counter() 
+static int static_counter()
 {
     static int count = 0; 
-    return ++count;      
+    return ++count;     
 }
