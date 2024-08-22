5aacfe61c1b1349820811db8307ebed7d844ff4c
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-08-22 14:38:24 +0900

    Retry1

--- exercise9/practice_9.c
+++ exercise9/practice_9.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*値を入れ替える関数のプロトタイプ*/
-void swap_int(int *a, int *b);
+static void swap_int(int *a, int *b); 
 
 int main(void) 
 {
     int x = 1;
     int y = 2;
 
-    
     printf("入れ替え前: x = %d, y = %d\n", x, y);
     
     /*swap_int関数を呼び出して値を入れ替え*/
@@ -19,8 +17,16 @@ int main(void)
     return 0;
 }
 
-/*値を入れ替える関数の定義*/
-void swap_int(int *a, int *b) {
+/**
+ * @brief 値を入れ替える関数
+ *
+ * この関数は、2つの整数のポインタを受け取り、これらの値を入れ替えます。
+ *
+ * @param a 値を入れ替える整数へのポインタ
+ * @param b 値を入れ替える整数へのポインタ
+ */
+static void swap_int(int *a, int *b) 
+{
     int temp = *a;  
     *a = *b;        
     *b = temp;      
