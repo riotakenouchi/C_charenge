8c260ef4b105d35992e1bf2dabd9dfd335e1c3c4
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-08-26 15:11:44 +0900

    課題10のコードと実行結果

---
+++ exercise10/practice_10.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/** @brief データ構造体 */
+typedef struct DAT_t 
+{
+    int n;                /**< 整数の値 */
+    const char* s;       /**< 文字列 */
+} DAT;
+
+/** @brief 比較関数
+ *
+ * @note 2つの DAT 構造体を比較し、qsort で使用するための関数。
+ *       n の値で昇順にソートし、n が同一の場合は s の文字コードで昇順にソート。
+ *
+ * @param[in,out] a 比較する最初の要素
+ * @param[in,out] b 比較する次の要素
+ * @return 整数 比較結果
+ */
+static int compare(const void *a, const void *b) 
+{
+    DAT *datA = (DAT *)a;
+    DAT *datB = (DAT *)b;
+
+    if (datA->n != datB->n) {
+        return datA->n - datB->n;
+    } else {
+        return strcmp(datA->s, datB->s);
+    }
+}
+
+/** @brief メイン関数
+ *
+ * DAT 構造体の配列をソートして表示します。
+ */
+int main(void) 
+{
+    DAT dat[] =
+     {
+        {9, "A"},
+        {2, "A"},
+        {5, "AB"},
+        {5, "AA"},
+    };
+
+    size_t size = sizeof(dat) / sizeof(dat[0]);
+
+    /* qsort を使用して配列をソート */
+    qsort(dat, size, sizeof(DAT), compare);
+
+    /* ソートされた配列を表示 */
+    for (size_t i = 0; i < size; i++) {
+        printf("n: %d, s: %s\n", dat[i].n, dat[i].s);
+    }
+
+    return 0;
+}
+
