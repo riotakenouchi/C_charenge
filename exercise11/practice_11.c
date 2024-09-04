10928d297c75a75b4e4b9f52f53a850f1b700891
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-09-04 14:37:38 +0900

    Retry2およびファイルの追加(sample.txt)

--- exercise11/practice_11.c
+++ exercise11/practice_11.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 256  /* バッファのサイズを定義 */
+
 int main(int argc, char *argv[]) 
 {
-    /* ファイルのポインタと文字を格納する変数を宣言 */
-    FILE *file_pointer; 
-    int character;
+    /* ファイルのポインタとバッファを宣言 */
+    FILE *fp = NULL; 
+    char buffer[BUFFER_SIZE];
+    const char *input_filename = argv[1]; 
 
     /* 引数の数を確認 */
     if (argc != 2) {
-        fprintf(stderr, "usage\nusage: display_file filename\n", argv[0]);
+        fprintf(stderr, "usage: display_file filename\n");
         return EXIT_FAILURE;
     }
 
-    /* 指定されたファイル名 */
-    const char *input_filename = argv[1]; 
-
     /* ファイルを開く */
-    file_pointer = fopen(input_filename, "r"); 
-    if (file_pointer == NULL) {
+    fp = fopen(input_filename, "r"); 
+    if (fp == NULL) {
         perror("Error opening file");
         return EXIT_FAILURE;
     }
 
     /* ファイルの内容を表示 */
-    while ((character = fgetc(file_pointer)) != EOF) {
-        putchar(character);
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        /* fgetsで読み込んだ文字列を表示 */
+        printf("%s", buffer);
     }
 
     /* ファイルを閉じる */
-    fclose(file_pointer); /* 変数名を file_pointer に変更 */
+    if (fclose(fp) != 0) {
+        perror("Error closing file");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
 
