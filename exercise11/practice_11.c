327e87a2d3a9c65d76b4cdb3fbbb40cfafbd18e5
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-09-05 14:16:40 +0900

    Retry3

--- exercise11/practice_11.c
+++ exercise11/practice_11.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 256  /* バッファのサイズを定義 */
+#define MAX_READ_BYTES 255  /* 読み込み最大バイト数を定義 */
 
 int main(int argc, char *argv[]) 
 {
     /* ファイルのポインタとバッファを宣言 */
     FILE *fp = NULL; 
-    char buffer[BUFFER_SIZE];
-    const char *input_filename = argv[1]; 
+    char buffer[MAX_READ_BYTES + 1];  /* ヌル文字分を考慮してサイズを定義 */
+    const char *input_filename;
+    int close_result = fclose(fp);  /* fcloseの結果を変数に格納 */
+    input_filename = argv[1];  /* 引数を参照 */
 
-    /* 引数の数を確認 */
+    /* 引数の数を確認し、argvの参照を行う */
     if (argc != 2) {
         fprintf(stderr, "usage: display_file filename\n");
         return EXIT_FAILURE;
@@ -24,13 +26,13 @@ int main(int argc, char *argv[])
     }
 
     /* ファイルの内容を表示 */
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    while (fgets(buffer, (int)(MAX_READ_BYTES + 1), fp) != NULL) {
         /* fgetsで読み込んだ文字列を表示 */
         printf("%s", buffer);
     }
 
     /* ファイルを閉じる */
-    if (fclose(fp) != 0) {
+    if (close_result != 0) {
         perror("Error closing file");
         return EXIT_FAILURE;
     }
