2c84060def9af6a5b40d59ba22598bf2c9496522
rio_takenouchi <rio_takenouhci@saison-technology.com>
2024-09-10 14:30:26 +0900

    Retry10

--- exercise11/practice_11.c
+++ exercise11/practice_11.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #define MAX_READ_BYTES 384  /* 読み込み最大バイト数を定義（ページサイズの倍数） */
 #define SUCCESS 0           /* プログラム成功時の戻り値 */
 #define FAILURE 1           /* プログラム失敗時の戻り値 */
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    char buffer[MAX_READ_BYTES + 1];  
-    const char *input_filename; 
-    FILE *fp = NULL; 
+    char buffer[MAX_READ_BYTES + 1];
+    const char *input_filename;
+    FILE *fp = NULL;
 
     /* 引数の数を確認 */
     if (argc != 2) {
@@ -20,10 +22,10 @@ int main(int argc, char *argv[])
     input_filename = argv[1];  /* 引数を参照 */
 
     /* ファイルを開く */
-    fp = fopen(input_filename, "r"); 
+    fp = fopen(input_filename, "r");
     if (fp == NULL) {
-        perror("Error opening file");
-        return FAILURE;
+        fprintf(stderr, "Error opening file '%s': %s\n", input_filename, strerror(errno));
+        return FAILURE;  /* エラー理由の出力 */
     }
 
     /* ファイルの内容を表示 */
@@ -41,7 +43,7 @@ int main(int argc, char *argv[])
     /* ファイルを閉じる */
     /* fcloseは常に実施する */
     if (fclose(fp) != 0) {
-        perror("Error closing file");  /*  fcloseの戻り値チェック */
+        perror("Error closing file");  /* fcloseの戻り値チェック */
         return FAILURE;
     }
 
